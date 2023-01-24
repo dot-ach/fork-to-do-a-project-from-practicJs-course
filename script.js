2cea6f359b216e20f4a93ba4b56b64aeeac14571
Alan <holasoyalan9@gmail.com>
2023-01-24 01:28:55 -0600

    Clase 19, Fusión del menú en mobile

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 console.log(navEmail);
+const burgerMenuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 const toggleDesktopMenu = () => {
   if(desktopMenu.className.includes('inactive')){
@@ -11,4 +13,10 @@ const toggleDesktopMenu = () => {
   console.log('click');
 };
 
+const toggleMobileMenu = () => {
+  mobileMenu.classList.toggle('inactive');
+  console.log('click')
+};
+
+burgerMenuIcon.addEventListener('click', toggleMobileMenu);
 navEmail.addEventListener('click', toggleDesktopMenu);
